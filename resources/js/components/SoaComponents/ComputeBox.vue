3a0738a39a6bdc027a356f1272d0fcd7ea89d8ee
Buddy1403 <marvreyes123@gmail.com>
2022-11-08 08:46:29 +0800

    fix computation under jcap

--- resources/js/components/SoaComponents/ComputeBox.vue
+++ resources/js/components/SoaComponents/ComputeBox.vue
@@ -144,7 +144,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label"
@@ -162,7 +162,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label">Draw*{{ commissionPercent * 100 }}% (kiosk):</span
@@ -179,7 +179,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label"
@@ -199,7 +199,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label"
@@ -251,7 +251,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label">Sales Deduction:</span>
@@ -269,7 +269,7 @@
                     </div>
 
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label"
@@ -290,7 +290,7 @@
                         </div>
                     </div>
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="custom-label"
@@ -312,7 +312,7 @@
                     </div>
 
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="font-weight-bold custom-label ctotal-label"
@@ -331,7 +331,7 @@
                     </div>
 
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="font-weight-bold custom-label ctotal-label"
@@ -351,7 +351,7 @@
                     </div>
 
                     <div
-                        v-if="computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com'"
+                        v-if="!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com')"
                         class="d-flex align-center justify-end computation-wrapper_field"
                     >
                         <span class="font-weight-bold custom-label ctotal-label"
@@ -429,7 +429,7 @@
             </div>
         </v-row>
         <v-row class="row-compute">
-            <div class="d-flex mobile-computebox" :style="[computation.arena_details.email_details[0].email != 'dea.greatpeak@gmail.com' ? {'margin-top': '-85px'} : {'margin-top': '50px'}]">
+            <div class="d-flex mobile-computebox" :style="[!computation.arena_details.email_details[0].email.includes('dea.greatpeak@gmail.com') ? {'margin-top': '-85px'} : {'margin-top': '50px'}]">
                 <div>
                     <div
                         class="d-flex justify-center align-center kiosk-mobile_text"
